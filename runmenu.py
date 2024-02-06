2185f54a1ce7d1e0e7657e59dc8858831a3bdbdd
SkyMoonIndustries <80589400+SkyMoonIndustries@users.noreply.github.com>
2024-02-07 01:33:09 +0300

    run menu

---
+++ runmenu.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import subprocess
+try:
+    cmd = "ps -t | grep skymenu.py | grep -v grep"
+    pid = subprocess.check_output(cmd, shell = True )
+    pid = pid.split(" ")
+    #print(pid[1])
+    pidnumber=pid[1]
+    pidname=pid[len(pid)-1]
+    #nb = len(pid)
+    #print(nb)
+    #print(pid[nb-1])
+    print(pidnumber + " => " + pidname)
+    cmd="kill "+ pidnumber + " &"
+    exe = subprocess.check_output(cmd, shell = True )
+    cmd="python3 /home/skymoon/SkyMenu/skymenu.py &"
+    exe = subprocess.check_output(cmd, shell = True )
+    exit()
+except:
+    print("no pid")
+    cmd="python3 /home/skymoon/SkyMenu/skymenu.py &"
+    exe = subprocess.check_output(cmd, shell = True )
+    exit()
